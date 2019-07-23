ec1a4505d054b602ee8ec092900f2a303ea88423
ekta <ektaadalja.silverwing@gmail.com>
2019-07-23 18:11:56 +0530

    23/07/2019 add utils file -> preference file and variablebag,network -> restcall and restclient

---
+++ app/src/main/java/com/example/eleven/activity/signInActivity.java
@@ -0,0 +1,16 @@
+package com.example.eleven.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.eleven.R;
+
+public class signInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
