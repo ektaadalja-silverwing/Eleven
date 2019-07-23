5d0bc94d91860be53620a712d3f0f6fe29325193
ekta <ektaadalja.silverwing@gmail.com>
2019-07-23 17:18:15 +0530

    23/07/2019 color file and style file

--- app/src/main/java/com/example/eleven/activity/MainActivity.java
+++ app/src/main/java/com/example/eleven/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.eleven;
+package com.example.eleven.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.eleven.R;
 
 public class MainActivity extends AppCompatActivity {
 
